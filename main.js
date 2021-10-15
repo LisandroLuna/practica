85d2791f120955f97abd2b52bb89fdfa3de66e5a
Lisandro Luna <f.lisandro.luna@gmail.com>
2021-10-15 00:31:59 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// Variables
+
+let variable = 'Hola'
+let numero = 9
+
+console.log(variable)  
+console.log(numero)  
+
+variable = 'Otro'
+numero = numero + 10
+
+console.log(variable)  
+console.log(numero)  
+
+// Constante
+
+const CONSTANTE = 1.21
+
+numero = numero * CONSTANTE
+
+// Concatenar
+
+console.log('Numero con IVA = ' + numero)
+
+
+let uno = '1'
+let dos = '2'
+
+
+console.log(uno + dos)  
+
